e6bb705757e0f1c64e907a1993b679c1dec43304
Ernesto <ernestoraf2011@gmail.com>
2024-01-02 20:01:32 -0600

    estructura de response

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -3,7 +3,14 @@ import { obtenerUsuariosBLL, insertarUsuariosBLL } from '../capas/BLL/usuarios';
 
 export const obtenerUsuarios = async ( req, res ) => {
     try {
-        await obtenerUsuariosBLL(req, res);
+        const data = await obtenerUsuariosBLL(req, res);
+        const response = {
+            status: 'Exito',
+            statusCode: 200,
+            datos: data
+        }
+        res.status(response.statusCode).send(response);
+
     } catch (error) {
         return res.status(400).json({ msg: "Bad request", error: error.message });
     }
