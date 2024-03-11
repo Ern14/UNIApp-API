03a266514d58234517b198b1847c20eb45a28d23
Ernesto <ernestoraf2011@protonmail.com>
2024-03-10 18:34:58 -0600

    CRUD DocenteGrupo

---
+++ src/capas/DAL/operaciones/DocenteGrupo.js
@@ -0,0 +1,64 @@
+import { getConnection, sql } from "../../../database/connection";
+
+export const obtenerDocenteGrupoDAL = async () => {
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().execute('[Operaciones].[FiltrarDocenteGrupo]');
+        return result.recordset;
+    } catch (error) {
+        throw error;
+    }
+
+};
+
+export const obtenerDocenteGrupoxIdDAL = async (idDocenteGrupo) => {
+    try {
+        const pool = await getConnection();
+        const result = await pool.request()
+        .input('IdDocenteGrupo' ,sql.Int, idDocenteGrupo)
+        .execute('[Operaciones].[FiltrarDocenteGrupoxId]');
+        return result.recordset;
+    } catch (error) {
+        throw error;
+    }
+
+};
+
+export const insertarDocenteGrupoDAL = async (modDocenteGrupo) => {
+    try {
+        const pool = await getConnection();
+        const result = await pool.request()
+        .input('IdDocente' ,sql.Int, modDocenteGrupo.idDocente)
+        .input('IdGrupo' ,sql.Int, modDocenteGrupo.idGrupo) 
+        .input('Activo' ,sql.Bit, modDocenteGrupo.Activo)
+        .input('FechaCreacion' ,sql.DateTime, modDocenteGrupo.FechaCreacion)
+        .input('UsuarioCreacion' ,sql.Int, modDocenteGrupo.UsuarioCreacion)
+        .input('FechaModificacion' ,sql.DateTime, modDocenteGrupo.FechaModificacion)
+        .input('UsuarioModificacion' ,sql.Int, modDocenteGrupo.UsuarioModificacion)
+        .execute('[Operaciones].[InsertarDocenteGrupo]');
+        return result.recordset;
+    } catch (error) {
+        throw error;
+    }
+
+};
+
+export const actualizarDocenteGrupoDAL = async (modDocenteGrupo) => {
+    try {
+        const pool = await getConnection();
+        const result = await pool.request()
+        .input('IdDocenteGrupo' ,sql.Int, modDocenteGrupo.idDocenteGrupo) 
+        .input('IdDocente' ,sql.Int, modDocenteGrupo.idDocente)
+        .input('IdGrupo' ,sql.Int, modDocenteGrupo.idGrupo)
+        .input('Activo', sql.Bit, modDocenteGrupo.Activo)
+        .input('FechaCreacion' ,sql.DateTime, modDocenteGrupo.FechaCreacion)
+        .input('FechaModificacion' ,sql.DateTime, modDocenteGrupo.FechaModificacion)
+        .input('UsuarioCreacion' ,sql.Int, modDocenteGrupo.UsuarioCreacion)
+        .input('UsuarioModificacion' ,sql.Int, modDocenteGrupo.UsuarioModificacion)
+        .execute('[Operaciones].[ActualizarDocenteGrupo]');
+        return result.recordset;
+    } catch (error) {
+        throw error;
+    }
+
+};
--- src/routes/operaciones/docenteGrupo.routes.js
+++ src/routes/operaciones/docenteGrupo.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
 
+import { obtenerDocenteGrupo, insertarDocenteGrupo, actualizarDocenteGrupo, eliminarDocenteGrupo } from "../../controllers/operaciones/DocenteGrupo.Controller";
+
 const router = Router();
+
+router.get('/operaciones/obtenerDocenteGrupo', obtenerDocenteGrupo);
+
+router.post('/operaciones/insertarDocenteGrupo', insertarDocenteGrupo);
+
+router.put('/operaciones/actualizarDocenteGrupo', actualizarDocenteGrupo);
+
+router.delete('/operaciones/eliminarDocenteGrupo', eliminarDocenteGrupo);
+
 
 export default router;
