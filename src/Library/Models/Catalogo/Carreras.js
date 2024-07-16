11ad772aa700549ecab8b2516b7ccdf54cbad233
Ernesto <ernestoraf2011@protonmail.com>
2024-07-15 22:35:32 -0600

    Avances

--- src/Library/Models/Catalogo/Carreras.js
+++ src/Library/Models/Catalogo/Carreras.js
@@ -5,6 +5,7 @@ export class Carreras{
         this.Abreviatura = userData.Abreviatura;
         this.Nombre = userData.Nombre;
         this.Descripcion = userData.Descripcion;
+        this.NombreDepartamento = userData.NombreDepartamento;
         this.Activo = userData.Activo;
         this.FechaCreacion = userData.FechaCreacion;
         this.FechaModificacion = userData.FechaModificacion;
